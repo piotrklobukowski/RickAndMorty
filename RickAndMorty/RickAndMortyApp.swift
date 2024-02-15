46a79d3857bf928586887ad75cb0e493aa4b09c9
Piotr Kłobukowski <piotr.klobukowski@gmail.com>
2024-02-15 18:06:49 +0100

    Add project files

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Piotr Kłobukowski on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
