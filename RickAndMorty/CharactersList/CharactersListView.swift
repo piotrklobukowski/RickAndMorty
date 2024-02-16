933420b02eaafd738675d7869538c589a4df68cb
Piotr Kłobukowski <piotr.klobukowski@gmail.com>
2024-02-16 10:05:52 +0100

    Rename ContentView into CharactersListView

--- RickAndMorty/CharactersList/CharactersListView.swift
+++ RickAndMorty/CharactersList/CharactersListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  CharactersListView.swift
 //  RickAndMorty
 //
 //  Created by Piotr Kłobukowski on 15/02/2024.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CharactersListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    CharactersListView()
 }
